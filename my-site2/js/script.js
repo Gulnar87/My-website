2e76d86c43b45754f69cf4bd4a6b4459efe38377
Gulnar87 <gulnar_mammadova@yahoo.com>
2018-05-04 16:23:31 +0400

    change

--- my-site2/js/script.js
+++ my-site2/js/script.js
@@ -1,4 +1,23 @@
 
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+
 $(document).ready(function(){
 $('.autoplay').slick({
   
@@ -158,6 +177,7 @@ $(document).ready(function(){
     }) 
   }) 
 })
+
 
 $('.demo').circleGraphic({color:'#00a99d'});
 
